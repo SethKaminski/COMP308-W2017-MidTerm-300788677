cfe12126f6beebd68d84ecf467ae08777361b5f0
SethKaminski <sethgabe@yahoo.com>
2017-02-25 09:09:56 -0500

    added delete

--- server/routes/books.js
+++ server/routes/books.js
@@ -89,10 +89,15 @@ router.post('/:id', (req, res, next) => {
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+let id = req.params.id;
+  book.remove({_id: id}, (err) => {
+    if (err) {
+      console.error(err);
+      res.end(error);
+    } else {
+        res.redirect('/books')
+    }
+  });
 });
 
 
